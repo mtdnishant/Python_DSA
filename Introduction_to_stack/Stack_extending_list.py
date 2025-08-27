8a48cfc8dd2cae59fba935cff19b4e0a04fc8fd9
Nishant_kumar <mtdnishant123@gmail.com>
2025-08-27 20:23:16 +0530

    stack extend list

---
+++ Introduction_to_stack/Stack_extending_list.py
@@ -0,0 +1,33 @@
+class Stack(list):
+    
+    def is_empty(self):
+        return len(self) == 0 
+    
+    def  push(self,data):
+        self.append(data)
+    def pop(self):
+        if not self.is_empty():
+            return super().pop() 
+        else:
+            raise IndexError("Stack is empty")  
+        
+    def peek(self):
+        if not self.is_empty():
+            return self[-1]
+        else:
+            IndexError("Stack is empty...")
+    
+    def size(self):
+        return len(self)
+    
+    def insert(self,index,data):
+        raise AttributeError("No attribute 'insert' in Stack")
+        
+s1 = Stack()
+s1.push(10)
+s1.push(20)
+s1.push(30)
+s1.push(90)
+print("top value",s1.peek())
+        
+        
